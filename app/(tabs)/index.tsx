d640fe6b4ba9aeb0578c487162d290de6a2276a9
Hamilton <dehamiltonglobal@gmail.com>
2025-06-08 02:30:49 +0100

    homePageNavs

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,10 @@
+// import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-primary font-bold">This is Hamilton.</Text> 
+      <Text className="text-5xl text-dark-200  font-bold">This is Hamilton.</Text> 
     </View>
   );
 }
