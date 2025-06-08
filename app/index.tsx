46a0d757c4daf2178972438d701096885a0c0054
Hamilton <dehamiltonglobal@gmail.com>
2025-06-08 01:12:26 +0100

    running

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>This is Hamilton. We Live!!!</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl text-primary font-bold">This is Hamilton.</Text> 
     </View>
   );
 }
