21911f6c8c7ddf1d83fbcf8f7b85e02713a017eb
Hamilton <dehamiltonglobal@gmail.com>
2025-06-08 22:57:37 +0100

    atIt

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,28 +1,97 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { Tabs } from 'expo-router'
+import { Tabs } from "expo-router";
+import { ImageBackground, Image, View } from "react-native";
+
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+
+function TabIcon({ focused, icon }: any) {
+  if (focused) {
+    return (
+      <ImageBackground
+        className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
+        source={images.highlight}
+      >
+        <Image source={icon} tintColor="#151312" className="size-5" />
+      </ImageBackground>
+    );
+  }
+
+  return (
+    <View className="size-full justify-center items-center mt-4 rounded-full">
+      <Image source={icon} tintColor="#A8B5DB" className="size-5" />
+    </View>
+  );
+}
 
 const _Layout = () => {
   return (
-    <Tabs>
-        <Tabs.Screen 
-            name='index'
-            options={{ 
-                title: 'Home',
-                headerShown: false 
-            }}
-        />
-        <Tabs.Screen 
-            name='saved'
-            options={{ 
-                title: 'Saved',
-                headerShown: false 
-            }}
-        />
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarItemStyle: {
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        },
+        tabBarStyle: {
+          backgroundColor: "#0F0D23",
+          borderRadius: 50,
+          marginHorizontal: 20,
+          marginBottom: 36,
+          height: 52,
+          position: "absolute",
+          overflow: "hidden",
+          borderWidth: 1,
+          borderColor: "#0F0D23",
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "index",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.home} title="Home" />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: "Search",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.search} title="Search" />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="saved"
+        options={{
+          title: "Saved",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.save} title="Save" />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.person} title="Profile" />
+          ),
+        }}
+      />
     </Tabs>
-  )
+  );
 }
 
-export default _Layout
-
-const styles = StyleSheet.create({})
+export default _Layout;
